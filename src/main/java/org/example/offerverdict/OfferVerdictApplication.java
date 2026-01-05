6fd19ca3eea0f4061b6ea2ca595f8a17f531bcc9
shinhyeok <shinhyeok22@gmail.com>
2026-01-05 17:21:30 +0900

    init

---
+++ src/main/java/org/example/offerverdict/OfferVerdictApplication.java
@@ -0,0 +1,13 @@
+package org.example.offerverdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferVerdictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferVerdictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/offerverdict/OfferVerdictApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.offerverdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferVerdictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
